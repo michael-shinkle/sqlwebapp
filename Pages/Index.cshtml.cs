79f214568413edcb3ceb40634e778c56708ef33e
LloydTechnologies <ralphblloyd@yahoo.com>
2023-08-25 13:31:22 -0400

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using splwebapp.Models;
+using splwebapp.Services;
+
+namespace splwebapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product> Products;
+        public void OnGet()
+        {
+            ProductService productService = new ProductService();
+            Products = productService.GetProducts();
+        }
+    }
+}
